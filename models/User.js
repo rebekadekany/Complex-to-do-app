67d66b8de2290d5ce127eaaa0efd0e1832bf66cf
Becca <o.dekany@gmail.com>
2020-07-14 17:43:31 +0200

    creating and validating login

--- models/User.js
+++ models/User.js
@@ -48,6 +48,17 @@ User.prototype.validate = function () {
     }
 }
 
+User.prototype.login = function(callback) {
+    this.cleanUp()
+    usersCollection.findOne({username: this.data.username}, (err, attemptedUser) => {
+        if (attemptedUser && attemptedUser.password == this.data.password) {
+            callback("Congrats!")
+        } else {
+            callback("Invalid username / password")
+        }
+    })
+}
+
 User.prototype.register = function () {
     //step #1 validate user data
     this.validate()
